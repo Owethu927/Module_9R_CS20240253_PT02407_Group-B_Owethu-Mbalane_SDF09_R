642d7a6bcc31559fcf7229eb02892cd9a0df268b
Owethu927 <owethusilela@gmail.com>
2024-09-05 20:05:47 +0100

    Added a stylesheet file

--- index.js
+++ index.js
@@ -5,7 +5,21 @@
 
 let firstCard = 6
 let secondCard = 9
-
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 console.log(sum)
+
+if (sum < 21) {
+    message ="Do you want to draw a new card?"
+} else if (sum === 21) {
+    massage ="Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+} else{
+    message ="You're out of the game"
+    isAlive = false
+}
+
+console.log(message)
