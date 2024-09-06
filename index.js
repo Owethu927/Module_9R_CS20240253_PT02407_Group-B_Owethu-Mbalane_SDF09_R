68428e6397eda955c2a7ef44f2abebbd3a3cd39d
Owethu927 <owethusilela@gmail.com>
2024-09-06 20:56:40 +0100

    added some math for random number

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ function getRandomCard() {
     let randomNumber = Math.floor(Math.random()*13) + 1
     if (randomNumber >10) {
         return 10
+    } else if (randomNumber === 1) {
+        return 11
     } else {
         return randomNumber
     }
