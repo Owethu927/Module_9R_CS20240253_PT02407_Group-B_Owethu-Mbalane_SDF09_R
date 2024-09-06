17a9810fc9681034ed5c0678ec54469395df0498
Owethu927 <owethusilela@gmail.com>
2024-09-06 18:56:05 +0100

    created a new variable to create an easier way of saying the massages

--- index.js
+++ index.js
@@ -10,19 +10,22 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el") // Created a variable to change the way the massage is asked//
+console.log(messageEl)
+
 // creating the button function//
 function startGame() {
     if (sum < 21) {
         message ="Do you want to draw a new card?"
     } else if (sum === 21) {
-        massage ="Wohoo! You've got Blackjack!"
+        massage ="You've got Blackjack!"
         hasBlackJack = true
     } else{
         message ="You're out of the game"
         isAlive = false
     }
     
-    console.log(message)
+    messageEl.textContent = message
 }
 
 
